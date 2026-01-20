08a8452e6fa1818d8b9630ee0f78e53267db490f
Shivam Ranabhat <shivamranabhat@gmail.com>
2026-01-20 21:46:11 +0000

    Added featured mobile view

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -24,6 +24,30 @@ $(document).ready(function () {
             }
         }
     });
+
+    $(".featured-slider").owlCarousel({
+        loop: true,
+        center: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        smartSpeed: 1000,
+        autoplaySpeed: 1000,
+        nav: false,
+        dots: false,
+        responsive: {
+            1200: {
+                items: 3.4
+            },
+            768: {
+                items: 2.4
+            },
+            0: {
+                items: 1.4
+            }
+        }
+    });
+
     $(".activity-slider").owlCarousel({
         items: 5,
         margin:30,
